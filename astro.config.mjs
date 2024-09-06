df30d271d75d1a58d7698308ffe82c36cf50128f
CLXD <lalobolito196@gmail.com>
2024-09-06 14:30:46 -0600

    update some things and use vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import tailwind from "@astrojs/tailwind";
 
 import preact from "@astrojs/preact";
 
+import vercel from "@astrojs/vercel/static";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
+  output: "static",
+  adapter: vercel(),
 });
