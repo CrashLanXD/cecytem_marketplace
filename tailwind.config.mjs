505c7540a70756420aebbbdedcaacb760b03b0ce
CLXD <lalobolito196@gmail.com>
2024-11-13 22:19:47 -0600

    add toggle theme option with tailwind darkMode selector, and fix some details

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,7 @@ import typography from '@tailwindcss/typography'
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'selector',
 	theme: {
 		extend: {
 			fontFamily: {
