d8517702cb7042bf65f234ec27f39a349e151b32
CLXD <lalobolito196@gmail.com>
2024-11-13 20:09:35 -0600

    lol

--- src/constants/team-members.ts
+++ src/constants/team-members.ts
@@ -1,11 +1,15 @@
-interface teamMember {
+export interface teamMember {
   name: string;
   role: string;
   pfp: string;
-  motto: string;
-  imgLoading?: "eager" | "lazy";
   imgWidth: number;
   imgHeight: number;
+  imgLoading?: "eager" | "lazy";
+  motto: string;
+  secondaryPfp: string;
+  secondaryImgWidth?: number;
+  secondaryImgHeight?: number;
+  secondaryImgLoading?: "eager" | "lazy";
   socialMedia: {
     x: string;
     github: string;
@@ -19,6 +23,7 @@ export const TEAM_MEMBERS: teamMember[] = [
     name: "Eduardo Sanchez",
     role: "Web developer | UI/UX Designer",
     pfp: "/images/team/clxd.webp",
+    secondaryPfp: "/images/team/clxd-2.webp",
     motto: "If you want it all, why you halfway in?.",
     imgLoading: "eager",
     imgWidth: 636,
@@ -34,6 +39,7 @@ export const TEAM_MEMBERS: teamMember[] = [
     name: "David",
     role: "Backend developer",
     pfp: "/images/team/david.webp",
+    secondaryPfp: "/images/team/david-2.webp",
     motto: "The best way to predict the future is to invent it",
     imgLoading: "eager",
     imgWidth: 460,
@@ -49,6 +55,7 @@ export const TEAM_MEMBERS: teamMember[] = [
     name: "Gamaliel",
     role: "collaborator",
     pfp: "/images/team/gama.webp",
+    secondaryPfp: "/images/team/gama-2.webp",
     motto: "Irure labore ea exercitation deserunt duis pariatur cillum ea Lorem.",
     imgLoading: "eager",
     imgWidth: 309,
@@ -64,6 +71,7 @@ export const TEAM_MEMBERS: teamMember[] = [
     name: "Diego",
     role: "collaborator",
     pfp: "/images/team/diego.webp",
+    secondaryPfp: "/images/team/diego-3.webp",
     motto: "La verdadera grandeza radica en la capacidad de levantarse después de cada caída.",
     imgWidth: 785,
     imgHeight: 785,
@@ -78,6 +86,7 @@ export const TEAM_MEMBERS: teamMember[] = [
     name: "Jocsan",
     role: "Collaborator",
     pfp: "/images/team/jocsan.webp",
+    secondaryPfp: "/images/team/jocsan-2.webp",
     motto: "Lo que tú haces en un día, yo lo hago en 24 horas.",
     imgWidth: 318,
     imgHeight: 318,
